6601109478caea5db281ee9a6c9c66931fcb38aa
Jeffrey Newman <jeff@newman.me>
2019-01-12 11:10:35 -0600

    Bump version: 1.0.2 → 1.0.3

--- conda_update.sh
+++ conda_update.sh
@@ -3,7 +3,7 @@
 export CONDA_RECIPE_DIR="conda-recipe"
 export CONDA_BUILD_DIR="conda-build"
 export CONDA_PKG_NAME="filez"
-export CONDA_PKG_VERSION="1.0.2"
+export CONDA_PKG_VERSION="1.0.3"
 
 conda build $CONDA_RECIPE_DIR --output-folder $CONDA_BUILD_DIR -c jpn --python 3.6
 conda build $CONDA_RECIPE_DIR --output-folder $CONDA_BUILD_DIR -c jpn --python 3.7
